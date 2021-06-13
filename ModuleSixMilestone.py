8c6c911c1e527f406c175fe0640f6cfe18d8386f
David Vega <dvega920@gmail.com>
2021-06-12 21:53:06 -0400

    Initial setup file

---
+++ ModuleSixMilestone.py
@@ -0,0 +1,11 @@
+# David Vega
+# Southern New Hampshire University
+# IT-140 - Intro to Scripting
+
+#A dictionary for the simplified dragon text game
+#The dictionary links a room to other rooms.
+rooms = {
+        'Great Hall': {'South': 'Bedroom'},
+        'Bedroom': {'North': 'Great Hall', 'East': 'Cellar'},
+        'Cellar': {'West': 'Bedroom'}
+    }
